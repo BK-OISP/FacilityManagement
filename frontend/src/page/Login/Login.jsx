913fad98e6ab38ee86a7dcd4f323c7eca3f79841
kevin <dqtkien@gmail.com>
2020-09-24 09:04:18 +0700

    finish login page

--- frontend/src/page/Login/Login.jsx
+++ frontend/src/page/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Tilt from "react-tilt";
 
 import OISP_Logo from "./OISP_Logo.png";
 
@@ -11,14 +12,19 @@ const LoginPage = () => {
       <Container className="login">
         <Row className="justify-content-between">
           <Col md={5} className="d-none d-md-block login--logo">
-            <img className="img-fluid" src={OISP_Logo} alt="OISP_Logo" />
+            <Tilt className="Tilt" options={{ max: 25, perspective: 600 }}>
+              <img className="img-fluid" src={OISP_Logo} alt="OISP_Logo" />
+            </Tilt>
           </Col>
           <Col md={7}>
             <Row className="justify-content-center login--header">
               Sign In With
             </Row>
             <Row className="justify-content-center mt-4">
-              <button className="btn login--google" id="button">
+              <button
+                className="btn btn-outline-light login--google "
+                id="button"
+              >
                 <img
                   width="40px"
                   style={{ margin: "10px" }}
