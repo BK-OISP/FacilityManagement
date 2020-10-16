e661b3fc139d1f7e7e38dcc558dfe112eee9b4c6
kevin <dqtkien@gmail.com>
2020-10-16 10:38:50 +0700

    finish transfer card

--- frontend/src/compoment/card/todocard/TodoCard.jsx
+++ frontend/src/compoment/card/todocard/TodoCard.jsx
@@ -4,18 +4,18 @@ import { Row, Col } from "antd";
 const TodoCard = (props) => {
   const { type, icon, header, text } = props;
   return (
-    <Col sm={12} md={6} className={`counter bg-${type} text-center`}>
-      <Row>
+    <Col sm={12} md={6} className={`counter bg-${type}`}>
+      <Row justify="center">
         <Col>
           <span className="material-icons">{icon}</span>
         </Col>
       </Row>
-      <Row className="counter--header">
+      <Row className="counter--header" justify="center">
         <Col>
           <h3>{header}</h3>
         </Col>
       </Row>
-      <Row>
+      <Row justify="center">
         <Col>
           <p>{text}</p>
         </Col>
