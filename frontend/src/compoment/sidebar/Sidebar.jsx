d948912aae4045f394a5ca6e3c21375d1e2b6cb3
kevin <dqtkien@gmail.com>
2020-10-26 15:54:00 +0700

    add layout roles

--- frontend/src/compoment/sidebar/Sidebar.jsx
+++ frontend/src/compoment/sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { useCallback } from "react";
 import { Link, NavLink } from "react-router-dom";
+import localStorageService from "../../helper/localStorage/localStorageService";
 
 const Sidebar = (props) => {
   const { menu } = props;
@@ -10,54 +12,71 @@ const Sidebar = (props) => {
     setToggleDropdown((pre) => !pre);
   };
 
+  const checkAuthorization = useCallback((appRoles) => {
+    const userRole = localStorageService.getRole();
+
+    return (
+      appRoles.role &&
+      appRoles.role.reduce((isAuthor, role) => {
+        return isAuthor && userRole.includes(role);
+      }, true)
+    );
+  }, []);
+
   return (
     <div className="sidebar overlay-scrollbar">
       <ul className="sidebar-nav">
         {menu.map((item) => {
           if (!item.sub) {
             return (
-              <li className="sidebar-nav--item" key={item.key}>
-                <div className="sidebar-nav--link">
-                  <NavLink to={item.link}>
-                    <div>
-                      <i className="material-icons">{item.icon}</i>
-                    </div>
-                    <span>{item.title}</span>
-                  </NavLink>
-                </div>
-              </li>
+              checkAuthorization(item) && (
+                <li className="sidebar-nav--item" key={item.key}>
+                  <div className="sidebar-nav--link">
+                    <NavLink to={item.link}>
+                      <div>
+                        <i className="material-icons">{item.icon}</i>
+                      </div>
+                      <span>{item.title}</span>
+                    </NavLink>
+                  </div>
+                </li>
+              )
             );
           } else {
             return (
-              <li className="sidebar-nav--item" key={item.key}>
-                <div className="sidebar-nav--link sidebar-nav--dropdown">
-                  <Link to="#" onClick={handleDropdown}>
-                    <div>
-                      <i className="material-icons">dashboard</i>
-                    </div>
-                    <span>{item.title}</span>
-                    <span
-                      style={{
-                        color: "#727475",
-                        fontSize: "15px",
-                        paddingLeft: "5px",
-                      }}
-                      className="material-icons"
-                    >
-                      keyboard_arrow_down
-                    </span>
-                  </Link>
-                  <ul className={`${toggleDropdown ? "appear" : "hide"}`}>
-                    {item.sub.map((sub) => {
-                      return (
-                        <li key={sub.key}>
-                          <NavLink to={sub.link}>{sub.title}</NavLink>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </li>
+              checkAuthorization(item) && (
+                <li className="sidebar-nav--item" key={item.key}>
+                  <div className="sidebar-nav--link sidebar-nav--dropdown">
+                    <Link to="#" onClick={handleDropdown}>
+                      <div>
+                        <i className="material-icons">dashboard</i>
+                      </div>
+                      <span>{item.title}</span>
+                      <span
+                        style={{
+                          color: "#727475",
+                          fontSize: "15px",
+                          paddingLeft: "5px",
+                        }}
+                        className="material-icons"
+                      >
+                        keyboard_arrow_down
+                      </span>
+                    </Link>
+                    {checkAuthorization(item) && (
+                      <ul className={`${toggleDropdown ? "appear" : "hide"}`}>
+                        {item.sub.map((sub) => {
+                          return (
+                            <li key={sub.key}>
+                              <NavLink to={sub.link}>{sub.title}</NavLink>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    )}
+                  </div>
+                </li>
+              )
             );
           }
         })}
@@ -66,4 +85,4 @@ const Sidebar = (props) => {
   );
 };
 
-export default Sidebar;
+export default React.memo(Sidebar);
--- frontend/src/page/MENU.jsx
+++ frontend/src/page/MENU.jsx
@@ -1,24 +1,31 @@
+import roles from "../helper/config/Roles";
+
 const MENU = [
   {
     key: "dashboard",
     title: "Dashboard",
     link: "/dashboard",
     icon: "dashboard",
+    role: [roles.FULLTIME],
   },
   {
     key: "facility",
     title: "Quản lý tài sản",
     icon: "dashboard",
+    role: [roles.FULLTIME],
+
     sub: [
       {
         key: "viewRequest",
         title: "Theo dõi đề xuất",
         link: "/facility/manage",
+        role: [roles.FULLTIME],
       },
       {
         key: "addRequest",
         title: "Thêm đề xuất",
         link: "/facility/add",
+        role: [roles.FULLTIME],
       },
     ],
   },
