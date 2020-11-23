42b7b3f4d8f870f69969f594c09cd1eff9091115
kevin <dqtkien@gmail.com>
2020-11-23 13:26:28 +0700

    add FMTeamlead management

--- frontend/src/page/facility/manageRequest/EditModal.jsx
+++ frontend/src/page/facility/manageRequest/EditModal.jsx
@@ -8,15 +8,54 @@ import {
   Button,
   Space,
   Image,
+  Divider,
 } from "antd";
 import { Field, Formik } from "formik";
 
 import CreateAntField from "../../../compoment/Form/CreateAntField/CreateAntField";
+import Roles from "../../../helper/config/Roles";
+import localStorageService from "../../../helper/localStorage/localStorageService";
+
+const HEAD_ROLE = [
+  Roles.FM_FACILITY_TEAM_LEAD,
+  Roles.FM_DEPUTY_HEAD,
+  Roles.FM_ADMIN_LEAD,
+  Roles.DIRECTOR,
+  Roles.ACCOUNTANT_LEAD,
+];
+
+const getCurrentRole = (userRole) => {
+  for (const role of userRole) {
+    if (HEAD_ROLE.includes(role)) {
+      return role;
+    }
+  }
+};
+
+const getCurrentRoleKey = (role) => {
+  switch (role) {
+    case Roles.FM_DEPUTY_HEAD:
+      return "isDeputyHeadApproval";
+    case Roles.FM_FACILITY_TEAM_LEAD:
+      return "isFMTeamLeadApproval";
+    case Roles.FM_ADMIN_LEAD:
+      return "isAccountLeadApproval";
+    case Roles.ACCOUNTANT_LEAD:
+      return "isAdminLeadApproval";
+    case Roles.DIRECTOR:
+      return "isDirectorApproval";
+    default:
+      break;
+  }
+};
 
 const EditModal = (props) => {
   const { setIsRerender, record, isModalOpen, setIsModalOpen } = props;
   const [totalPrice, setTotalPrice] = useState(0);
   const formRef = useRef();
+  const roleKey = getCurrentRoleKey(
+    getCurrentRole(localStorageService.getRole())
+  );
   console.log(record);
 
   const layout = {
@@ -26,19 +65,24 @@ const EditModal = (props) => {
 
   const initForm = {
     specs: record ? record.specs : "",
-    unit: record ? record.unit.label : "",
     unitPricePredict: record ? record.unitPricePredict : "",
-    note: record ? record.notes : "",
+    note: record && record.notes[roleKey] ? record.notes[roleKey] : "",
   };
 
   const handleTotalPrice = (value) => {
     const total = value * record.quantity;
-    setTotalPrice(total);
+    setTotalPrice(`${total}`.replace(/\B(?=(\d{3})+(?!\d))/g, ","));
   };
 
-  const handleSubmitForm = () => {};
+  const handleSubmitForm = () => {
+    console.log(formRef.current.values);
+    const check = parseInt(totalPrice);
+    console.log(typeof check);
+    console.log(check);
+  };
 
   const handleClose = () => {
+    setTotalPrice(0);
     setIsModalOpen(false);
   };
 
@@ -50,6 +94,7 @@ const EditModal = (props) => {
       centered
       footer={null}
       onCancel={handleClose}
+      destroyOnClose
     >
       <Descriptions title="Thông tin người đề xuất" bordered>
         <Descriptions.Item label="Tên nhân viên">
@@ -59,7 +104,132 @@ const EditModal = (props) => {
           {record.employeeId.department}
         </Descriptions.Item>
       </Descriptions>
-      <Descriptions title="Nội dung đề xuất" bordered className="mt-1">
+      <Divider orientation="center">Thông tin đề xuất</Divider>
+      <Row gutter={[48, 16]}>
+        <Col xs={24} sm={12}>
+          Tên danh mục: {record.fmName}
+        </Col>
+        <Col xs={24} sm={12}>
+          Loại danh mục: {record.fmBigGroup.label}
+        </Col>
+      </Row>
+      <Row gutter={[48, 16]}>
+        <Col xs={24} sm={12}>
+          Số lượng: {record.quantity}
+        </Col>
+        <Col xs={24} sm={12}>
+          Đơn vị tính: {record.unit.label}
+        </Col>
+      </Row>
+      <Row gutter={[48, 16]}>
+        <Col xs={24}>Mục đích: {record.purpose}</Col>
+      </Row>
+      <Row gutter={16} justify="center">
+        {record.imgCollection.map((item) => (
+          <Col key={item}>
+            <Image
+              width={150}
+              height={150}
+              src={`${process.env.REACT_APP_API_URL}/oisp/${item}`}
+            />
+          </Col>
+        ))}
+      </Row>
+      <Divider orientation="center">Thông tin bổ sung</Divider>
+
+      <Formik
+        initialValues={initForm}
+        onSubmit={handleSubmitForm}
+        // enableReinitialize={true}
+        innerRef={formRef}
+      >
+        {(handleSubmit, submitCount, values) => {
+          return (
+            <AntdForm onFinish={handleSubmitForm} {...layout}>
+              <Row gutter={[48, 16]}>
+                <Col xs={24} lg={12}>
+                  <Field
+                    component={CreateAntField}
+                    name="unitPricePredict"
+                    type="number"
+                    label="Đơn giá (dự kiến)*"
+                    submitCount={submitCount}
+                    hasFeedback
+                    setOtherValue={handleTotalPrice}
+                    style={{ width: "100%" }}
+                  />
+                </Col>
+                <Col xs={24} lg={12}>
+                  <Row>
+                    <Col span={8} style={{ textAlign: "right" }}>
+                      <strong>Thành tiền:</strong>
+                    </Col>
+                    <Col offset={1}>
+                      <strong>{totalPrice} VND</strong>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+              <Row gutter={[48, 16]}>
+                <Col xs={24} lg={12}>
+                  <Field
+                    component={CreateAntField}
+                    name="specs"
+                    type="textarea"
+                    label="Quy cách*"
+                    submitCount={submitCount}
+                    hasFeedback
+                  />
+                </Col>
+                <Col xs={24} lg={12}>
+                  <Field
+                    component={CreateAntField}
+                    name="note"
+                    type="textarea"
+                    label="Ghi chú"
+                    submitCount={submitCount}
+                    hasFeedback
+                  />
+                </Col>
+              </Row>
+              <Row justify="center" className="mb-1">
+                <Space>
+                  <Button type="primary" htmlType="submit">
+                    Duyệt đề xuất
+                  </Button>
+                  <Button type="primary" danger>
+                    Huỷ đề xuất
+                  </Button>
+                </Space>
+              </Row>
+              <Row justify="end" className="justify-content-sm-center">
+                <Space>
+                  <Button type="primary" className="btn-success">
+                    Lưu tạm
+                  </Button>
+                  <Button>Đóng</Button>
+                </Space>
+              </Row>
+            </AntdForm>
+          );
+        }}
+      </Formik>
+    </Modal>
+  );
+};
+
+export default EditModal;
+{
+  /* <Descriptions title="Thông tin người đề xuất">
+        <Descriptions.Item label="Tên nhân viên">
+          {record.employeeId.fullName}
+        </Descriptions.Item>
+        <Descriptions.Item label="Bộ phận">
+          {record.employeeId.department}
+        </Descriptions.Item>
+      </Descriptions>
+
+      <Descriptions title="Nội dung đề xuất" className="mt-1">
         <Descriptions.Item label="Tên danh mục">
           {record.fmName}
         </Descriptions.Item>
@@ -91,7 +261,7 @@ const EditModal = (props) => {
       <Formik
         initialValues={initForm}
         onSubmit={handleSubmitForm}
-        enableReinitialize={true}
+        // enableReinitialize={true}
         // innerRef={formRef}
       >
         {(handleSubmit, submitCount, values) => {
@@ -162,9 +332,5 @@ const EditModal = (props) => {
             </AntdForm>
           );
         }}
-      </Formik>
-    </Modal>
-  );
-};
-
-export default EditModal;
+      </Formik> */
+}
