dfaee6fffc9eaaa77aca185fc170bffc66fcd2e7
kevin <dqtkien@gmail.com>
2020-12-16 13:42:31 +0700

    fix bug

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -3,9 +3,9 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducer";
 
 const composeEnhancers =
-  process.env.NODE_ENV === "development"
+  (process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : null) || compose;
 
 const store = createStore(
   rootReducer,
